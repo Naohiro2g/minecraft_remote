e3726e42cd2dcf6182b8251580d1a51ce3ceeaf0
Naohiro Tsuji <naohiro2g@code2create.club>
2021-11-24 17:14:09 +0900

    param switching

--- maze.py
+++ maze.py
@@ -1,49 +1,62 @@
 # Maze builder in Minecraft world
 #
-# Written by PythonVTuberサプー
-# 動画：【Pythonでマイクラ操作！】自動で迷路を作成してマインクラフト上で遊んでみよう
+# Original code written by PythonVTuberサプー or PythonVTuber Supu
+# YouTube:【Pythonでマイクラ操作！】自動で迷路を作成してマインクラフト上で遊んでみよう
 #       〜 Minecraftプログラミング入門 〜    https://www.youtube.com/watch?v=iK3V8q2EiI8
-# Original code : https://colab.research.google.com/drive/1asGAz0ggpt4d-cVYPhyWkk_lTqTRNF-A
+# Original code: https://colab.research.google.com/drive/1asGAz0ggpt4d-cVYPhyWkk_lTqTRNF-A
 
 from mazelib import Maze
 from mazelib.generate.Prims import Prims
-from mcpi import minecraft, block
+# from mcpi import minecraft, block
+
+# Select modules to import here.
+# Minecraft Java Edition 1.16.5 : mcje, param_MCJE
+# Minecraft Java Edition 1.12.2 : mcpi, param_MCJE1122
+# Minecraft Pi Edition : mcpi, param_MCPI
+
+# from mcje.minecraft import Minecraft
+# import param_MCJE as param
+
+from mcpi.minecraft import Minecraft
+import param_MCJE1122 as param
+
+# from mcpi.minecraft import Minecraft
+# import param_MCPI as param
 
 N = 5
 M = 5
 H = 3
-SEA_LEVEL = 62
-SEA_LANTERN = 169
+
 
 def put_maze_block(mc, mark, x_pos, z_pos):
     if mark == '#':
         mc.setBlocks(
-            x_pos, SEA_LEVEL + 1, z_pos,
-            x_pos, SEA_LEVEL + 1 + H, z_pos,
-            SEA_LANTERN  # SEA_LANTERN block not available in mcpi
+            x_pos, param.Y_SEA + 1, z_pos,
+            x_pos, param.Y_SEA + 1 + H, z_pos,
+            param.SEA_LANTERN_BLOCK
         )
 
 
 def main():
-    mc = minecraft.Minecraft.create()
+    mc = Minecraft.create()
     mc.player.setPos(0, 80, 0)
 
     mc.setBlocks(
-        -40, SEA_LEVEL + 1, -40,
+        -40, param.Y_SEA + 1, -40,
         40, 127, 40,
-        block.AIR
+        param.AIR
     )
 
     mc.setBlocks(
-        -40, SEA_LEVEL - 1, -40,
-        40, SEA_LEVEL - 1, 40,
-        block.STONE
+        -40, param.Y_SEA - 1, -40,
+        40, param.Y_SEA - 1, 40,
+        param.STONE
     )
 
     mc.setBlocks(
-        -40, SEA_LEVEL, -40,
-        40, SEA_LEVEL, 40,
-        block.GRASS
+        -40, param.Y_SEA, -40,
+        40, param.Y_SEA, 40,
+        param.GRASS_BLOCK
     )
 
     mz = Maze()
@@ -58,7 +71,8 @@ def main():
     for z, mark_list in enumerate(maze_list):
         for x, mark in enumerate(mark_list):
             put_maze_block(mc, mark, x, z)
-            print(mark)
+            print(mark + mark, flush=False, end=' ')
+        print()
 
 
 if __name__ == '__main__':
