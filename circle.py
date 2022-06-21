b114df7feb9b1b46cdfd0f39fd0b0b3dd8034463
MqtiSub <103342043+MqtiSub@users.noreply.github.com>
2022-06-21 13:06:47 +0900

    Update circle.py
    Fixed for API.

--- circle.py
+++ circle.py
@@ -2,7 +2,7 @@ from mcje.minecraft import Minecraft
 import param_MCJE as param
 import numpy as np
 
-def setCircle(mc,radius):
+def setCircle(mc,ca,cb,radius):
     i=0
     rad=np.linspace(0,2*np.pi,5*radius)
     cx=radius*np.cos(rad[i])
@@ -10,8 +10,8 @@ def setCircle(mc,radius):
     int(cx,cz)
     while i < 100:
         i += 1
-        mc.setBlock(cx,70,cz,param.DIAMOND_BLOCK)
+        mc.setBlock(cx+ca,70,cz+cb,param.DIAMOND_BLOCK)
         
 if __name__ == '__main__'
     mc = Minecraft.create(port=param.PORT_MC)
-    setCircle(mc,8)
+    setCircle(mc,50,50,8)
