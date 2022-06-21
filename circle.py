cdb3fc3e1d56162153f678e4f0f2a145b02a46e7
MqtiSub <103342043+MqtiSub@users.noreply.github.com>
2022-06-21 12:56:07 +0900

    Update circle.py
    Enhance for API

--- circle.py
+++ circle.py
@@ -1,21 +1,17 @@
 from mcje.minecraft import Minecraft
 import param_MCJE as param
-from time import sleep
 import numpy as np
 
-def setCircle(wait=0.1):
-    mc = Minecraft.create(port=param.PORT_MC)
-    mc.postToChat("succeed?")
-    radius=8
+def setCircle(mc,radius):
     i=0
-    rad=np.linspace(0,2*np.pi,100)
+    rad=np.linspace(0,2*np.pi,5*radius)
     cx=radius*np.cos(rad[i])
     cz=radius*np.sin(rad[i])
     int(cx,cz)
     while i < 100:
         i += 1
         mc.setBlock(cx,70,cz,param.DIAMOND_BLOCK)
-        sleep(wait)
-        mc.postToChat(i)
         
-setCircle()
+if __name__ == '__main__'
+    mc = Minecraft.create(port=param.PORT_MC)
+    setCircle(mc,8)
